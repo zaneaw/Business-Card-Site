d2f35a7f7ee46f5efb2dd23a027ed38a968d771f
Zane Wilson <wilsonzanea@gmail.com>
2022-02-17 13:03:25 -0500

    links updated, margin/padding updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,18 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            <i className="fa-brands fa-twitter-square fa-2xl twitter"></i>
-            <i className="fa-brands fa-facebook-square fa-2xl facebook"></i>
-            <i className="fa-brands fa-instagram-square fa-2xl insta"></i>
-            <i className="fa-brands fa-github-square fa-2xl github"></i>
+            <a className="twitter" href="https://twitter.com/zane_ohh" target="_blank" rel="noreferrer">
+                <i className="fa-brands fa-twitter-square fa-2xl"></i>
+            </a>
+            <a className="facebook" href="https://www.facebook.com/zaneohh/" target="_blank" rel="noreferrer">
+                <i className="fa-brands fa-facebook-square fa-2xl"></i>
+            </a>
+            <a className="insta" href="https://www.instagram.com/zane_ohh/" target="_blank" rel="noreferrer">
+                <i className="fa-brands fa-instagram-square fa-2xl"></i>
+            </a>
+            <a className="github" href="https://github.com/zaneaw" target="_blank" rel="noreferrer">
+                <i className="fa-brands fa-github-square fa-2xl github"></i>
+            </a>
         </footer>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,20 @@ export default function Header() {
             <img src="assets/images/zane-big.jpg" alt="Zane" />
             <h3>Zane A. Wilson</h3>
             <h5>Full Stack Developer</h5>
-            <a href="https://www.zane-wilson.com/">
-                zane-wilson.com
-            </a>
+            <a href="https://www.zane-wilson.com/" target="_blank" rel="noreferrer">zane-wilson.com</a>
             <div className="button--div">
-                <button type="button" className="header--button em--button">
-                    <i className="fa-solid fa-envelope"></i>
-                    <p>Email</p>
-                </button>
-                <button type="button" className="header--button li--button">
-                    <i className="fa-brands fa-linkedin"></i>
-                    <p>LinkedIn</p>
-                </button>
+                <a href="mailto:wilsonzanea@gmail.com" target="_blank" rel="noreferrer">
+                    <button type="button" className="header--button em--button">
+                        <i className="fa-solid fa-envelope"></i>
+                        <p>Email</p>
+                    </button>
+                </a>
+                <a href="https://www.linkedin.com/in/zaneaw/" target="_blank" rel="noreferrer">
+                    <button type="link" className="header--button li--button">
+                        <i className="fa-brands fa-linkedin"></i>
+                        <p>LinkedIn</p>
+                    </button>
+                </a>
             </div>
         </div>
     );
